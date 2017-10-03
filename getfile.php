133faedd8201e8371efc1012cfe431c6b9d06c6b
pantgr <pantelis@protonmail.ch>
2017-10-03 12:38:49 +0300

    Add files via upload

--- getfile.php
+++ getfile.php
@@ -15,10 +15,10 @@ if (isset($_GET['e2'])) {
     echo "#NAME FOS-Streaming \r\n";
     foreach ($user->categories as $category) {
         foreach ($category->streams as $stream) {
-            if ($stream->running == 1) {
+            //if ($stream->running == 1) {
                 echo "#SERVICE 1:0:1:0:0:0:0:0:0:0:http%3A//" . $setting->webip . "%3A" . $setting->webport . "/live/" . $user->username . "/" . $user->password . "/" . $stream->id . "\r\n";
                 echo "#DESCRIPTION " . $stream->name . "\r\n";
-            }
+            //}
         }
     }
     header('Content-Type: application/octet-stream');
@@ -35,10 +35,10 @@ if (isset($_GET['m3u'])) {
     foreach ($user->categories as $category) {
         foreach ($category->streams as $stream) {
 
-            if ($stream->running == 1) {
+            //if ($stream->running == 1) {
                 echo "#EXTINF:0," . $stream->name . "\r\n";
                 echo "http://" . $setting->webip . ":" . $setting->webport . "/live/" . $user->username . "/" . $user->password . "/" . $stream->id . "\r\n";
-            }
+            //}
         }
     }
     header('Content-Type: application/octet-stream');
@@ -53,9 +53,9 @@ if (isset($_GET['m3u'])) {
 if (isset($_GET['tv'])) {
     foreach ($user->categories as $category) {
         foreach ($category->streams as $stream) {
-            if ($stream->running == 1) {
+            //if ($stream->running == 1) {
                 echo "ext,$stream->name,http://" . $setting->webip . ":" . $setting->webport . "/live/" . $user->username . "/" . $user->password . "/" . $stream->name . "\r\n";
-            }
+            //}
         }
     }
 
